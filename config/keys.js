117362c700ca5336ec0c8d0f80bbf87a3b0f4b2f
jesseflores512 <jesseflores512@gmail.com>
2018-07-31 04:56:56 -0500

    setup routes

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://jesse1:jesse1@ds159631.mlab.com:59631/devconnect'
+}
--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profiles');
+const posts = require('./routes/api/posts');
 
 const app = express();
 
+const db = require('./config/keys').mongoURI;
+
+mongoose
+  .connect(db)
+  .then(()=> console.log('Mongodb connected'))
+  .catch(err => console.log(err));
+
 app.get('/',(req, res) => res.send("Hell World"));
+
+//use routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
